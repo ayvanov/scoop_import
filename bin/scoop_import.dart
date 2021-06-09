0659f9c3830c5f2094b3957f52779a875714e493
Alexander Ivanov <xanderinho@gmail.com>
2021-06-09 14:06:15 -0700

    added comments check

--- bin/scoop_import.dart
+++ bin/scoop_import.dart
@@ -1,7 +1,7 @@
 import 'dart:io' show Process, File;
 import 'package:args/args.dart';
 
-void runScoop(List<String> args) async {
+Future runScoop(List<String> args) async {
   final process = await Process.run('powershell', ['scoop', ...args]);
   if (process.exitCode == 0) {
     print(process.stdout);
@@ -23,23 +23,34 @@ void main(List<String> arguments) async {
       final lines =
           String.fromCharCodes(bytes.buffer.asUint16List()).split('\n');
       for (var line in lines) {
-        final lineArr = line.split(' ');
-        if (lineArr.length == 3) {
-          final bucket = lineArr[2].substring(1, lineArr[2].length - 2);
-          apps.add([bucket, lineArr[0]].join('/'));
-          buckets.add(bucket);
+        final split = line.split(' ');
+        if (split.length == 3) {
+          final bucket = split[2].substring(1, split[2].length - 2);
+          final app = split[0];
+          final skip = [1058995764, 872292574].contains(split[0][0].hashCode);
+          if (!skip) {
+            if (bucket != 'main') {
+              apps.add([bucket, app].join('/'));
+              buckets.add(bucket);
+            } else {
+              apps.add(app);
+            }
+          }
         }
       }
     }
     if (buckets.isNotEmpty) {
+      //TODO check if installed
+      await runScoop(['install', 'git']);
       for (var bucket in buckets) {
-        runScoop(['install', 'git']);
-        runScoop(['bucket', 'add', bucket]);
+        //TODO check if added
+        await runScoop(['bucket', 'add', bucket]);
       }
     }
     if (apps.isNotEmpty) {
       for (var app in apps) {
-        runScoop(['install', app]);
+        //TODO check if installed
+        await runScoop(['install', app]);
       }
     }
   }
