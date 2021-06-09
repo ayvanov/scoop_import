0ce659c58169847d887c852ed8c95b3fd8e8207b
Alexander Ivanov <xanderinho@gmail.com>
2021-06-09 22:18:14 +0300

    init

---
+++ bin/scoop_import.dart
@@ -0,0 +1,50 @@
+import 'dart:io' show Process, File;
+import 'package:args/args.dart';
+
+void main(List<String> arguments) async {
+  final argParser = ArgParser();
+  final args = argParser.parse(arguments);
+  if (args.rest.isNotEmpty) {
+    final path = args.rest.toList().first;
+    final file = File(path);
+    final buckets = <String>{};
+    final apps = <String>[];
+    if (file.existsSync()) {
+      final bytes = file.readAsBytesSync();
+      final lines =
+          String.fromCharCodes(bytes.buffer.asUint16List()).split('\n');
+      for (var line in lines) {
+        final lineArr = line.split(' ');
+        if (lineArr.length == 3) {
+          final bucket = lineArr[2].substring(1, lineArr[2].length - 2);
+          apps.add([bucket, lineArr[0]].join('/'));
+          buckets.add(bucket);
+        }
+      }
+      // print(apps.toString());
+      // print(buckets.toString());
+    }
+    if (buckets.isNotEmpty) {
+      for (var bucket in buckets) {
+        final process =
+            await Process.run('powershell', ['scoop', 'bucket', 'add', bucket]);
+        if (process.exitCode == 0) {
+          print(process.stdout);
+        } else {
+          print(process.stderr);
+        }
+      }
+    }
+    if (apps.isNotEmpty) {
+      for (var app in apps) {
+        final process =
+            await Process.run('powershell', ['scoop', 'install', app]);
+        if (process.exitCode == 0) {
+          print(process.stdout);
+        } else {
+          print(process.stderr);
+        }
+      }
+    }
+  }
+}
